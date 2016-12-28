40a2e3f9023a7b0b07c3ed43cb00fff90e9a70f1
Kapil Kale <kapil.kale.07@gmail.com>
2016-12-27 22:43:42 -0500

    Styles

---
+++ lib/giftrocket/style.rb
@@ -0,0 +1,31 @@
+module Giftrocket
+  class Style
+
+    include HTTParty
+    base_uri "#{Giftrocket.config[:base_api_uri]}styles/"
+
+    attr_accessor :id, :card
+
+    def initialize(attributes)
+      attributes = attributes.with_indifferent_access
+      self.id = attributes[:id]
+      self.card = attributes[:card]
+    end
+
+    def url
+      card && card[:url]
+    end
+
+    def self.list
+      response = get '/', query: Giftrocket.default_options, format: 'json'
+      if response.success?
+        response_json = JSON.parse(response.body).with_indifferent_access
+        response_json[:styles].map do |style_attributes|
+          ::Giftrocket::Style.new(style_attributes)
+        end
+      else
+        raise Giftrocket::Error.new(response)
+      end
+    end
+  end
+end
---
+++ test/test_style.rb
@@ -0,0 +1,54 @@
+require 'minitest/autorun'
+require 'webmock/minitest'
+require 'giftrocket'
+
+class StyleTest < Minitest::Test
+  def test_the_test
+    source = Giftrocket::Style.new({})
+    assert_equal "hello world", "hello world"
+  end
+
+  def test_initialization
+    data = {
+      "id": "ABCD23424",
+      "card": {
+        "url": "https://giftrocket.imgix.net/[asset_path]/thank_you_tree.jpg"
+      }
+    }.with_indifferent_access
+    style = Giftrocket::Style.new(data)
+    assert_equal data[:id], style.id
+    assert_equal data[:card], style.card
+    assert_equal data[:card][:url], style.url
+  end
+
+  describe 'requires config' do
+    before do
+      ::Giftrocket.configure do |config|
+        config[:access_token] = 'abcd'
+      end
+    end
+
+    def test_list
+      response = {
+        "styles": [
+          {
+            "id": "ABCD23424",
+            "card": {
+              "url": "https://giftrocket.imgix.net/[asset_path]/thank_you_tree.jpg"
+            }
+          }
+        ]
+      }
+
+      stub_request(:get, 'https://www.giftrocket.com/api/v1/styles/').
+        with(query: Giftrocket.default_options).
+        to_return(
+          status: 200,
+          body: response.to_json,
+          headers: {"Content-Type"=> "application/json"}
+        )
+      styles = Giftrocket::Style.list
+      assert_equal styles.count, 1
+    end
+  end
+end
